c208638f65dc40d5585cc4c262a78d20c28e7eef
Nicola Vianello <nicola.vianello@gmail.com>
2020-01-15 10:24:12 +0900

    blackify python script

--- python/figureIaea-synopsis.py
+++ python/figureIaea-synopsis.py
@@ -55,7 +55,7 @@ mpl.rc("font", **{"family": "sans-serif", "sans-serif": ["Tahoma"]})
 def print_menu():
     print(30 * "-", "MENU", 30 * "-")
     print("1. Equilibria explored")
-    print("2. Upstream profiles and target conditions")
+    print("2. Upstream profiles at two different recycling state")
     print("99: End")
     print(67 * "-")
 
@@ -214,8 +214,149 @@ while loop:
         fig.savefig("../pdfbox/AllEquilibria.pdf", bbox_to_inches="tight")
 
     elif selection == 2:
-        pass
+        # create the figure
+        fig, ax = mpl.pylab.subplots(figsize=(13, 5), nrows=1, ncols=3, sharey="row")
+        fig.subplots_adjust(bottom=0.15, wspace=0.05, right=0.98)
+        for _idax, (_ax, exp) in enumerate(zip(ax, ("JET", "AUG", "TCV"))):
+            _ax.set_yscale("log")
+            _ax.set_xlabel(r"$\rho$")
+            _ax.set_ylim([0.05, 3])
+            _ax.set_xlim([0.95, 1.15])
+            if _idax != 0:
+                _ax.tick_params(labelleft=False)
+            else:
+                _ax.set_ylabel(r"n$_e$/n$_e^{\rho=1}$")
+            _ax.set_title(exp)
 
+        # ---------------------
+        # --- JET -------------
+        # ---------------------
+        shotList = (95504, 95641)
+        colorList = ("#0568A6", "#F29F05")
+        label = ("Low Rec", "High Rec")
+        for _idxshot, (shot, col, lb) in enumerate(zip(shotList, colorList, label)):
+            # determine the corresponding average Te
+            jsonfile = "/Users/nicolavianello/Desktop/M18-41/Analysis/data/shot{}-VT5C.json".format(
+                shot
+            )
+            Dictionary = {}
+            with open(jsonfile, "r") as f:
+                Dictionary = json.load(f)
+            SpecTe = tespec.tespec(shot, configuration="VT5C", dtype="telo")
+            t, y = SpecTe.selectradius()
+            te = np.nanmean(
+                y[np.logical_and(t >= Dictionary["tmin"], t <= Dictionary["tmax"])]
+            )
+            # now load the saved profiles
+            File = "/Users/nicolavianello/Desktop/M18-41/Analysis/data/Shot{}_VT5C.npz".format(
+                shot
+            )
+            Data = np.load(File)
+            _norm = Data["enfit"][np.argmin(np.abs(Data["rhofit"] - 1))]
+            ax[0].errorbar(
+                Data["rho"],
+                Data["ne"] / _norm,
+                yerr=Data["dne"] / _norm,
+                fmt="o",
+                color=col,
+                alpha=0.3,
+                ms=5,
+                errorevery=5,
+            )
+            ax[0].plot(Data["rhofit"], Data["enfit"] / _norm, "-", lw=2, color=col)
+            ax[0].fill_between(
+                Data["rhofit"],
+                (Data["enfit"] - Data["denfit"]) / _norm,
+                (Data["enfit"] + Data["denfit"]) / _norm,
+                alpha=0.05,
+                color=col,
+            )
+            ax[0].text(
+                0.45, 0.9 - 0.1 * _idxshot, lb, transform=ax[0].transAxes, color=col,
+            )
+            # ax[0].text(
+            #     0.45,
+            #     0.9 - 0.1 * _idxshot,
+            #     r"$\langle$T$_e\rangle$ = {:.1f} eV".format(te),
+            #     transform=ax[0].transAxes,
+            #     color=col,
+            # )
+
+        # ---------------------
+        # --- AUG -------------
+        # ---------------------
+        FileList = (
+            "../data/AUG/ProfileShot34276_t2.25-2.35.npz",
+            "../data/AUG/ProfileShot34276_t5.40-5.50.npz",
+        )
+        for _idxshot, (file, col, lb) in enumerate(zip(FileList, colorList, label)):
+            Data = np.load(file)
+            _norm = Data["rawEn"][np.argmin(np.abs(Data["rawRho"] - 1))]
+            if _idxshot == 1:
+                _e = Data["rawEn_err"] / 2.0
+            else:
+                _e = Data["rawEn_err"]
+            ax[1].errorbar(
+                Data["rawRho"],
+                Data["rawEn"] / _norm,
+                xerr=Data["rawRho_err"],
+                yerr=_e / _norm,
+                fmt="-o",
+                color=col,
+                alpha=0.5,
+                ms=5,
+            )
+            ax[1].text(
+                0.45, 0.9 - 0.1 * _idxshot, lb, transform=ax[1].transAxes, color=col,
+            )
+            # ax[1].text(
+            #     0.45,
+            #     0.9 - 0.1 * _idxshot,
+            #     r"$\langle$T$_e\rangle$ = {:.1f} eV".format(Data["teosp"]),
+            #     transform=ax[1].transAxes,
+            #     color=col,
+            # )
+
+        # ---------------------
+        # --- TCV -------------
+        # ---------------------
+        FileList = (
+            "../data/TCV/ProfileShot64494_t0.88-0.96.npz",
+            "../data/TCV/ProfileShot64947_t1.50-1.75.npz",
+        )
+        for _idxshot, (file, col, lb) in enumerate(zip(FileList, colorList, label)):
+            Data = np.load(file)
+            _norm = Data["fitEn"][np.argmin(np.abs(Data["fitRho"] - 1))]
+            ax[2].errorbar(
+                Data["rawRho"],
+                Data["rawEn"] / _norm,
+                yerr=Data["rawEn_err"] / _norm,
+                fmt="o",
+                color=col,
+                alpha=0.5,
+                ms=5,
+            )
+            ax[2].plot(Data["fitRho"], Data["fitEn"] / _norm, "-", color=col, lw=2)
+            ax[2].fill_between(
+                Data["fitRho"],
+                (Data["fitEn"] - Data["fitEn_err"]) / _norm,
+                (Data["fitEn"] + Data["fitEn_err"]) / _norm,
+                color=col,
+                alpha=0.3,
+            )
+
+            ax[2].text(
+                0.45, 0.9 - 0.1 * _idxshot, lb, transform=ax[2].transAxes, color=col,
+            )
+            # ax[2].text(
+            #     0.45,
+            #     0.9 - 0.1 * _idxshot,
+            #     r"$\langle$T$_e\rangle$ = {:.1f} eV".format(Data["teosp"]),
+            #     transform=ax[2].transAxes,
+            #     color=col,
+            # )
+
+        fig.savefig("../pdfbox/AllUpstreamProfiles_synopsis.pdf")
     elif selection == 99:
         loop = False
     else:
